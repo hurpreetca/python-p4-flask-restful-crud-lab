3984306e7d4e19ac26e54bb786388084ffa76498
Harpreet Singh <hurpreet@icloud.com>
2023-09-26 23:24:08 -0400

    Lab Completed

--- server/app.py
+++ server/app.py
@@ -46,6 +46,25 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        for attr in request.get_json():
+            setattr(plant, attr, request.get_json()[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+        dict= plant.to_dict()
+        response= make_response(dict, 200)
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        message = {"message": "delete complete"}
+        response = make_response(message, 204)
+        return response
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
